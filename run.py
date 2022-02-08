964bdeaf78642ecdceedd1018f6a007b132dbbb8
Muhammad Zainul umam <93089530+Shinzy24@users.noreply.github.com>
2022-02-08 22:12:47 +0700

    Update run.py

--- run.py
+++ run.py
@@ -1,7 +1,5 @@
 if __name__ == "__main__":
-   try:
-       __import__("app").hencet_memek()
-   except Exception as e:
-       exit(str(e))
-
-#contibuttors Zainul umam
+	try:
+		__import__("app").make()
+	except Exception as e:
+		exit(str(e))
